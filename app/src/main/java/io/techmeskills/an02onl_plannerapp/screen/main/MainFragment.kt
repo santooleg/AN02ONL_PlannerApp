33a42a3ef0e7818f6f2d080beba8124d5f1a61e1
Aleh Alkchimovich <o.alkchimovich@gmail.com>
2021-03-23 18:16:32 +0300

    Home work 1

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
@@ -1,15 +1,13 @@
 package io.techmeskills.an02onl_plannerapp.screen.main
 
-import android.os.Build
 import android.os.Bundle
 import android.view.View
-import androidx.annotation.RequiresApi
 import by.kirich1409.viewbindingdelegate.viewBinding
 import io.techmeskills.an02onl_plannerapp.R
 import io.techmeskills.an02onl_plannerapp.databinding.FragmentMainBinding
 import io.techmeskills.an02onl_plannerapp.support.NavigationFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.lang.Integer.sum
+
 
 class MainFragment : NavigationFragment<FragmentMainBinding>(R.layout.fragment_main) {
 
@@ -17,38 +15,16 @@ class MainFragment : NavigationFragment<FragmentMainBinding>(R.layout.fragment_m
 
     private val viewModel: MainViewModel by viewModel()
 
-    var count: Int = 0
-        set(value) {
-            field = value
-            viewBinding.tvCount.text = field.toString()
-        }
-    var count2: Int = 0
-        set(value) {
-            field = value
-            viewBinding.tvCount2.text = field.toString()
-        }
-
-    var count3: Int = 0
-        set(value) {
-            field = value
-            viewBinding.tvCount3.text = field.toString()
-        }
-
-    override fun onInsetsReceived(top: Int, bottom: Int, hasKeyboard: Boolean) {
-
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewBinding.btnClicker.setOnClickListener {
-            count++
-        }
-        viewBinding.btnClicker2.setOnClickListener {
-            count2++
-        }
-        viewBinding.btnClicker3.setOnClickListener {
-            count3 = count + count2
-        }
+
+        viewBinding.recyclerView.adapter = NotesRecyclerViewAdapter(viewModel.notes)
+    }
+
+    override fun onInsetsReceived(top: Int, bottom: Int, hasKeyboard: Boolean) {
+        viewBinding.toolbar.setPadding(0, top, 0, 0)
+        viewBinding.recyclerView.setPadding(0, 0, 0, bottom)
+
     }
 
 }
---
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/NotesRecyclerViewAdapter.kt
@@ -0,0 +1,37 @@
+package io.techmeskills.an02onl_plannerapp.screen.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.techmeskills.an02onl_plannerapp.R
+
+class NotesRecyclerViewAdapter(private val items: List<Note>) :
+        RecyclerView.Adapter<NoteViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.note_list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
+
+class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
+    private val tvDate = itemView.findViewById<TextView>(R.id.tvDate)
+
+    fun bind(item: Note){
+        tvTitle.text = item.title
+        tvDate.text = item.date
+
+    }
+}
